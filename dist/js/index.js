0518b558cf5474754e0830c80daa0565df48475a
GMarqz <gabrielmarques9782@gmail.com>
2024-12-09 12:24:06 -0300

    index.ts

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,3 +2,11 @@ import ui from "./ui.js";
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizaPets();
 });
+const $caesBtn = document.getElementById("caes-btn");
+const $gatosBtn = document.getElementById("gatos-btn");
+$caesBtn.addEventListener(("click"), () => {
+    console.log("Cães");
+});
+$gatosBtn.addEventListener(("click"), () => {
+    console.log("Gatos");
+});
