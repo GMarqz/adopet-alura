22a4df49c328be58e0d7599e132289e073e6852d
GMarqz <gabrielmarques9782@gmail.com>
2024-12-09 12:23:31 -0300

    add eventListener para os botões

--- app/index.ts
+++ app/index.ts
@@ -4,3 +4,14 @@ import ui from "./ui.js"
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizaPets();
 });
+
+const $caesBtn = document.getElementById("caes-btn");
+const $gatosBtn = document.getElementById("gatos-btn");
+
+$caesBtn.addEventListener(("click"), () => {
+    console.log("Cães");
+});
+
+$gatosBtn.addEventListener(("click"), () => {
+    console.log("Gatos");
+})
